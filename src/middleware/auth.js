563e8542d26835dd459a8d236b3a9a2e656430c8
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-24 10:37:34 +0100

    feature: verify password

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,12 @@
 const bcrypt = require("bcrypt");
 
+const User = require("../users/model");
+
 const saltRounds = parseInt(process.env.SALT_ROUNDS);
 
 const hashPass = async (req, res, next) => {
   try {
-    console.log("pass before hash: ", req.body.password);
     const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-    console.log("hashed pass: ", hashedPassword);
 
     req.body.password = hashedPassword;
 
@@ -16,4 +16,27 @@ const hashPass = async (req, res, next) => {
   }
 };
 
-module.exports = { hashPass: hashPass };
+const comparePass = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.body.username },
+    });
+
+    if (!user) {
+      res.status(404).json({ message: "user not found" });
+    }
+
+    const compare = await bcrypt.compare(req.body.password, user.password);
+
+    if (!compare) {
+      res.status(404).json({ message: "incorrect password" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+module.exports = { hashPass: hashPass, comparePass: comparePass };
--- src/users/routes.js
+++ src/users/routes.js
@@ -2,16 +2,16 @@ const { Router } = require("express");
 const userRouter = Router();
 
 const { signup, login, getAllUsers } = require("./controllers");
-// const { hashPass } = require("../middleware/auth");
+const { hashPass, comparePass } = require("../middleware/auth");
 
 userRouter.get("/test", async (req, res) => {
   res.status(200).json({ message: "test okey dokey!" });
 });
 
-userRouter.post("/signup", signup);
+userRouter.post("/signup", hashPass, signup);
 //removed hashPass for login test- put back
 
-userRouter.post("/login", login);
+userRouter.post("/login", comparePass, login);
 
 userRouter.get("/getAllUsers", getAllUsers);
 
